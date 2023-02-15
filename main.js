0b342b197d9074bb153665fe5ea1a67e68defa06
CJ.Sandor <71267879+123csandor@users.noreply.github.com>
2023-02-15 07:43:35 -0500

    Upload of all my code

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
+
+})
+
+app.mount('#app');
