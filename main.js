e11c42faabb62f7593e0fb3f77d8cfb7a2dafd8c
CJ.Sandor <71267879+123csandor@users.noreply.github.com>
2023-02-16 09:48:55 -0500

    Completed Part Two
    Learned about for each loops and v-if's with vue.

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
     data() {
         return{
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            showBooks: true
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            showBooks: true,
+            // x: 0,
+            // y: 0
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'patrick rothfuss'},
+            ],
+            url: "http://www.csandor.com",
         }
     },
     methods: {
@@ -14,6 +22,16 @@ const app = Vue.createApp({
         },
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data){
+            console.log(e, e.type);
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 
